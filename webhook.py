94327568d3d216b6b179c73e271ba22de30afb53
Peter McGarrigle <mcgarrigle@github.com>
2024-02-09 18:15:21 +0000

    POST /keys redirects to /certs

--- webhook.py
+++ webhook.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect
 from . import ssh_ca
 
 base = os.path.dirname(os.path.realpath(__file__))
@@ -27,16 +27,15 @@ def webhook_post_key():
     token = request.headers["token"]
     with open(f"users/{token}.pub", "w") as f:
         f.write(request.data.decode("utf-8"))
-    resp = Response()
-    resp.status = '201'
-    return resp
+    return redirect(f"/certs/{token}", code=302)
 
-@app.route("/certs", methods=['GET'])
-def webhook_get_cert():
+@app.route("/certs/<id>", methods=['GET'])
+def webhook_get_cert(id):
     token = request.headers["token"]
+    print(f"token = {token}")
     profile = ca.profile(token)
     if profile == {}:
         return reply(None)
     else:
-        cert = sign(token, profile)
+        cert = sign(id, profile)
         return reply(cert)
