c5db3dacfc3d630ac51199b0fe1f46de7ca27dfe
Peter McGarrigle <mcgarrigle@github.com>
2024-02-11 16:14:32 +0000

    working with sshkey-tools

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 import os
 import json
-import ssh_ca
-import hashlib
 import functools
-from flask import Flask, request, Response, redirect
+from flask import Flask, request, Response
+import ssh_ca
 
 base = os.path.dirname(os.path.realpath(__file__))
 ca = ssh_ca.ca(base)
@@ -24,29 +23,27 @@ def authenticate(func):
     @functools.wraps(func)
     def wrapper_decorator(*args, **kwargs):
         token = request.headers.get("token")
-        if not token: return Response('Token Missing', 401)
+        if not token:
+            return Response('Token Missing', 401)
         profile = ca.profile(token)
-        if not profile: return Response('Invalid Token', 401)
+        if not profile:
+            return Response('Invalid Token', 401)
         value = func(*args, **kwargs)
         return value
     return wrapper_decorator
 
-def sign(digest, profile):
-    certfile = ca.sign(f"keys/{digest}.pub", profile)
-    with open(certfile, "r") as f:
-        return f.read()
-
 @app.route("/keys", methods=['POST'])
 @authenticate
 def service_post_key():
-    digest = ca.store_key(request.data)
+    digest = ca.store_public_key(request.data)
     result = { "id": digest, "href": f"/certs/{digest}" }
     return reply(result, 201)
 
 @app.route("/certs/<digest>", methods=['GET'])
 @authenticate
 def service_get_cert(digest):
+    public_key = ca.retrieve_public_key(digest)
     profile = ca.profile(request.headers["token"])
-    certificate = sign(digest, profile)
+    certificate = ca.sign(public_key, profile)
     result = { "id": digest, "href": f"/certs/{digest}", "certificate": certificate }
     return reply(result)
